ea46eefdc804e82901d4080c0be334e2b3b808ce
sofijalazarova <94320367+sofijalazarova@users.noreply.github.com>
2023-12-21 16:12:44 +0100

    Course details page changes

--- galaxia/src/pages/CourseDetails.jsx
+++ galaxia/src/pages/CourseDetails.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { SectionWrapper } from '../hoc';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from '../custom-axios/axios';
 
 const CourseDetails = () => {
+
+    const navigate = useNavigate();
 
     const { id } = useParams();
     const [course, setCourse] = useState(null);
@@ -24,35 +26,94 @@ const CourseDetails = () => {
         return <div>Loading...</div>
     }
 
+    console.log(course);
+
   return (
 
-    <div className='mt-10'>
-        <div className='bg-tertiary rounded-2xl w-full h-[470px] flex items-center'>
-            <div className='relative w-[370px] h-[330px] mx-20'>
-                <img src={course.imagePath}
-                     alt="course_image"
-                     className='h-full w-full'
-                />
+    // <div className='mt-10'>
+    //     <div className='bg-tertiary rounded-2xl w-full h-[470px] flex items-center'>
+    //         <div className='relative w-[370px] h-[330px] mx-20'>
+    //             <img src={course.imagePath}
+    //                  alt="course_image"
+    //                  className='h-full w-full'
+    //             />
+    //         </div>
+    //         <div className='w-[550px] h-[330px] sm:mr-20 xs:mr-20 flex flex-col justify-start'>
+    //             <div className='items-center'>
+    //                 <h2 className='text-white font-bold text-[24px]'>{course.name}</h2>
+    //                 <p className='text-secondary text-[14px] mt-5'>{course.description}</p>
+    //                 <p className='text-secondary text-[14px] mt-2'>{course.duration} hours</p>
+    //                 <p className='text-secondary text-[14px] mt-2'>Includes video materials</p>
+    //             </div>
+    //             <div className='mt-5 h-full flex flex-col items-center justify-center'>
+    //                 <button
+    //                     onClick={() => {navigate("/course/" + course.id + "/lessons")}}
+    //                     className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+    //                 >
+    //                     Start course
+    //                 </button>
+    //             </div>
+                
+    //         </div>
+    //     </div>
+    // </div>
+//     <div className='mt-10 xs:m-10'>
+//     <div className='bg-tertiary rounded-2xl w-full h-[570px] flex flex-col-reverse sm:flex-row items-center'>
+//         <div className='relative w-full sm:w-[370px] h-[330px] mx-20 sm:mb-0'>
+//             <img
+//                 src={course.imagePath}
+//                 alt="course_image"
+//                 className='h-full w-full'
+//             />
+//         </div>
+//         <div className='w-full sm:w-[550px] h-[330px] sm:mr-20 flex flex-col justify-start mt-10'>
+//             <div className='items-center text-center'>
+//                 <h2 className='text-white font-bold text-[24px]'>{course.name}</h2>
+//                 <p className='text-secondary text-[14px] mt-5'>{course.description}</p>
+//                 <p className='text-secondary text-[14px] mt-2'>{course.duration} hours</p>
+//                 <p className='text-secondary text-[14px] mt-2'>Includes video materials</p>
+//             </div>
+//             <div className='mt-5 mb-10 h-full flex flex-col items-center justify-center'>
+//                 <button
+//                     onClick={() => {navigate("/course/" + course.id + "/lessons")}}
+//                     className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+//                 >
+//                     Start course
+//                 </button>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+
+<div className='mt-10 xs:m-10'>
+    <div className='bg-tertiary rounded-2xl w-full h-[550px] flex flex-col-reverse md:flex-row items-center'>
+        <div className='relative w-full md:w-[370px] h-[330px] mx-20 md:mb-0'>
+            <img
+                src={course.imagePath}
+                alt="course_image"
+                className='h-full w-full'
+            />
+        </div>
+        <div className='w-full md:w-[550px] h-[330px] md:mr-20 flex flex-col justify-start mt-10'>
+            <div className='items-center text-center'>
+                <h2 className='text-white font-bold text-[24px]'>{course.name}</h2>
+                <p className='text-secondary text-[14px] mt-5'>{course.description}</p>
+                <p className='text-secondary text-[14px] mt-2'>{course.duration} hours</p>
+                <p className='text-secondary text-[14px] mt-2'>Includes video materials</p>
             </div>
-            <div className='w-[550px] h-[330px] flex flex-col justify-start'>
-                <div className='items-center'>
-                    <h2 className='text-white font-bold text-[24px]'>{course.name}</h2>
-                    <p className='text-secondary text-[14px] mt-5'>{course.description}</p>
-                    <p className='text-secondary text-[14px] mt-2'>{course.duration} hours</p>
-                    <p className='text-secondary text-[14px] mt-2'>Includes video materials</p>
-                </div>
-                <div className='mt-20'>
+            <div className='mt-5 mb-10 h-full flex flex-col items-center justify-center'>
                 <button
-                    type='submit'
+                    onClick={() => {navigate("/course/" + course.id + "/lessons")}}
                     className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
                 >
                     Start course
-            </button>
-                </div>
-                
+                </button>
             </div>
         </div>
     </div>
+</div>
+
+
   )
 }
 
